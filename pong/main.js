99c33c690a965a864ba185d70dcac845ce7cff58
garciar316 <sergio980316@hotmail.com>
2022-04-28 13:48:33 -0500

    ADD: parte 4

--- pong/main.js
+++ pong/main.js
@@ -1,5 +1,5 @@
-(function() {
-    self.Board = function(width, height) {
+(function () {
+    self.Board = function (width, height) {
         this.width = width;
         this.height = height;
         this.playing = false;
@@ -15,10 +15,24 @@
             return elements;
         }
     }
-}) ();
+})();
 
 (function () {
-    self.Bar = function(x, y, width, height, board) {
+    self.Ball = function (x, y, radius, board) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.speed_x = 0;
+        this.speed_y = 3;
+        this.board = board;
+        this.kind = "circle";
+
+        board.ball = this;
+    }
+})();
+
+(function () {
+    self.Bar = function (x, y, width, height, board) {
         this.x = x;
         this.y = y;
         this.width = width;
@@ -27,23 +41,23 @@
         this.board = board;
         this.board.bars.push(this);
         this.kind = "rectangle";
-        this.speed = 10;
+        this.speed = 5;
     }
     self.Bar.prototype = {
-        down: function() {
+        down: function () {
             this.y += this.speed;
         },
-        up: function() {
+        up: function () {
             this.y -= this.speed;
         },
-        toSring: function() {
+        toSring: function () {
             return "x: " + this.x + ", y: " + this.y;
         }
     }
-}) ();
+})();
 
 (function () {
-    self.BoardView = function(canvas, board) {
+    self.BoardView = function (canvas, board) {
         this.canvas = canvas;
         this.canvas.width = board.width;
         this.canvas.height = board.height;
@@ -52,42 +66,62 @@
     }
 
     self.BoardView.prototype = {
-        draw: function() {
-            for (var i = this.board.elements.length - 1; i>= 0; i--) {
+        clean: function () {
+            this.ctx.clearRect(0, 0, this.board.width, this.board.height)
+        },
+        draw: function () {
+            for (var i = this.board.elements.length - 1; i >= 0; i--) {
                 var el = this.board.elements[i];
                 draw(this.ctx, el);
             }
+        },
+        play: function () {
+            this.clean();
+            this.draw();
         }
     }
 
     function draw(ctx, element) {
-        if (element !== null && element.hasOwnProperty("kind")) {
-            switch(element.kind) {
+        switch (element.kind) {
             case "rectangle":
                 ctx.fillRect(element.x, element.y, element.width, element.height);
                 break;
-            }
+            case "circle":
+                ctx.beginPath();
+                ctx.arc(element.x, element.y, element.radius, 0, 7);
+                ctx.fill();
+                ctx.closePath();
+                break;
         }
     }
-}) ();
+})();
 
 var board = new Board(800, 400);
 var bar = new Bar(20, 100, 40, 100, board);
+var bar_2 = new Bar(735, 100, 40, 100, board);
 var canvas = document.getElementById('canvas');
 var board_view = new BoardView(canvas, board);
+var ball = new Ball(350, 100, 10, board);
 
-document.addEventListener("keydown", function(event) {
+document.addEventListener("keydown", function (event) {
     if (event.key === "ArrowUp") {
         bar.up();
     }
     if (event.key === "ArrowDown") {
         bar.down();
     }
+    if (event.key === "w") {
+        bar_2.up();
+    }
+    if (event.key === "s") {
+        bar_2.down();
+    }
     console.log(bar.toSring());
 })
 
-self.addEventListener("load", main)
+window.requestAnimationFrame(controller);
 
-function main() {
-    board_view.draw();
+function controller() {
+    board_view.play();
+    window.requestAnimationFrame(controller);
 }
